3b6dc64f1ccbac9761a5c0299608d27cd1af0322
Aderinola Elesha <aderinolawilliams077@gmail.com>
2025-04-30 07:40:27 +0100

    Added Patient Log service file

--- src/PatientLogs/patient_log.controller.ts
+++ src/PatientLogs/patient_log.controller.ts
@@ -1,13 +1,15 @@
 import { Controller, Get, UseGuards } from "@nestjs/common";
 import { PatientLogService } from "./patient_log.service";
 import { PatientGuard } from "src/auth/guards/patient.guard";
+import { UserDecorator } from "src/auth/decorators/user.decorator";
+import { User } from "src/utils/types";
 
 @Controller()
 export class PatientLog{
     constructor(private patientLogService:PatientLogService){}
     @Get('patient-logs')
     @UseGuards(PatientGuard)
-    getAllPatientLogs(){
-        this.patientLogService.getAllPatientLogs()
+    getAllPatientLogs(@UserDecorator() user: User){
+        return this.patientLogService.getAllPatientLogs()
     }
 }
--- src/PatientLogs/patient_log.service.ts
+++ src/PatientLogs/patient_log.service.ts
@@ -1,6 +1,32 @@
-import { Injectable } from "@nestjs/common";
+import { HttpException, Injectable } from "@nestjs/common";
+import { PrismaService } from "src/prisma/prisma.service";
+import { User } from "src/utils/types";
 
 @Injectable()
 export class PatientLogService{
+    constructor(private prisma: PrismaService){
 
+    }
+    async getAllPatientLogs(user: User){
+        try {
+            const getPatientData = await this.prisma.user.findUnique({
+                where: {
+                  id: user.id,
+                },
+                select: {
+                  patient: true,
+                },
+              });
+        const logsData =  this.prisma.logsRecord.findMany({
+                where:{
+                    patientId: getPatientData.patient.id
+                }
+            }) 
+
+            return logsData;
+        } catch (error) {
+            throw new HttpException('Unable to fetch logs at this time', 400)
+        }
+      
+    }
 }
